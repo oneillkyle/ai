e9b225178f75513d05795f35750beadfab356824
Kyle O'Neill <oneillkyle@gmail.com>
2025-03-25 09:57:15 -0700

    initial code for ML sequences

--- src/tutorials/ml_sequences.py
+++ src/tutorials/ml_sequences.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import tensorflow as tf
 import keras
 
 
@@ -30,6 +31,16 @@ def noise(time, noise_level=1, seed=None):
     return rnd.randn(len(time)) * noise_level
 
 
+def windowed_dataset(series, window_size, batch_size, shuffle_buffer):
+    dataset = tf.data.Dataset.from_tensor_slices(series)
+    dataset = dataset.window(window_size + 1, shift=1, drop_remainder=True)
+    dataset = dataset.flat_map(lambda window: window.batch(window_size + 1))
+    dataset = dataset.shuffle(shuffle_buffer).map(
+        lambda window: (window[:-1], window[:-1]))
+    dataset = dataset.batch(batch_size).prefetch(1)
+    return dataset
+
+
 time = np.arange(4 * 365 + 1, dtype="float32")
 baseline = 10
 series = trend(time, .05)
@@ -44,8 +55,15 @@ series += seasonality(time, period=365, amplitude=amplitude)
 series += noise(time, noise_level, seed=42)
 
 split_time = 1000
-naive_forecast = series[split_time - 1: -1]
+time_train = time[:split_time]
+x_train = series[:split_time]
+time_valid = time[split_time:]
 x_valid = series[split_time:]
+window_size = 20
+batch_size = 32
+shuffle_buffer_size = 1000
+dataset = windowed_dataset(
+    x_train, window_size, batch_size, shuffle_buffer_size)
 
 plt.figure(figsize=(12, 6))
 plot_series(time, series)
